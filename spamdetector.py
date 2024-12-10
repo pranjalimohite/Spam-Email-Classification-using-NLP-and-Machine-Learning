a3426ddf609b4460e70aeb88f8d1904cadd8d31d
pranjalimohite <pranjalimohite2004@gmail.com>
2024-12-10 21:31:46 +0530

    Add files via upload

---
+++ spamdetector.py
@@ -0,0 +1,23 @@
+import streamlit as st
+import pickle
+model = pickle.load(open('spam.pkl','rb'))
+cv = pickle.load(open('vec.pkl','rb'))
+
+def main():
+    st.title("Email Spam Classification Application")
+    st.write("This is a Machine Learning application to classify email is spam or not spam")
+    st.subheader("Classification")
+    user_input = st.text_area("Enter the email for classify",height=150)
+    if st.button("Classify"):
+        if user_input:
+            data=[user_input]
+            print(data)
+            vec=cv.transform(data).toarray()
+            result=model.predict(vec)
+            if result[0]==0:
+                st.success("This is Not A Spam Email")
+            else:
+                st.error("This is A Spam Email")
+        else:
+            st.write("Please enter an email to classify.")
+main()
